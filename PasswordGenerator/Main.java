49d9a934603e2f8f5de1c412d7c2319580b20954
SynchDescronDev <SynchDescronDev@gmail.com>
2023-05-22 02:43:11 +0300

    Password Generator
    Can add a GUI and maybe use less symbols in the to improve the program.Right now looks functional enough.

---
+++ PasswordGenerator/Main.java
@@ -0,0 +1,40 @@
+package PasswordGenerator;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int userPassword;
+        
+        StringBuilder str = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+
+
+        for(int i=33;i<=126;i++){
+            str.append((char)i);
+        }
+      
+        System.out.println("Enter how many characters you want in your password");
+        userPassword = scanner.nextInt();
+        if(userPassword<1){
+            System.out.println("Please enter a value bigger than 1");
+        }
+        if(userPassword>=1){
+        System.out.println("Here is your randomly generated password");
+        for(int i=1;i<=userPassword;i++){
+                str2.setLength(userPassword);
+                str2.append(str.charAt(random.nextInt(93)+1));
+                System.out.print(str2);
+            }
+        }
+        
+
+        
+        scanner.close();
+      
+
+        }
+    }
