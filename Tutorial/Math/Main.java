2aa322bbfc50d277813c7117f035b8bc24679efd
SynchDescronDev <SynchDescronDev@gmail.com>
2023-05-23 18:23:50 +0300

    Math class

---
+++ Tutorial/Math/Main.java
@@ -0,0 +1,21 @@
+package Tutorial.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        double x = 3.14;
+        double y = -5;
+        double z = 25;
+
+        System.out.println(Math.abs(y));
+        System.out.println(Math.cbrt(z));
+        System.out.println(Math.ceil(x));
+        System.out.println(Math.floor(x));
+        System.out.println(Math.min(y, z));
+        System.out.println(Math.max(y, z));
+        System.out.println(Math.pow(z, x));
+        System.out.println(Math.sqrt(z));
+        System.out.println(Math.round(x));
+    
+
+    }
+}
