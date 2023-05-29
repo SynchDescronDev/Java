1d75c01affa38ae580765f36d4559cc2158c0fdc
SynchDescronDev <SynchDescronDev@gmail.com>
2023-05-29 20:25:18 +0300

    Update Main.java

--- Programs/PasswordGenerator/Main.java
+++ Programs/PasswordGenerator/Main.java
@@ -3,7 +3,7 @@ package Programs.PasswordGenerator;
 import java.util.Random;
 import java.util.Scanner;
 
-public class PasswordGenerator {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
